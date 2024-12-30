510af4a3c6d6f275724f3b2e17677740cb7aeb94
Verus0 <maphchbiea78@gmail.com>
2024-12-30 19:26:37 +0900

    회원 기능 추가

--- src/main/java/com/example/gdsc_2nd_w1/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gdsc_2nd_w1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUserName(String userName);
-}
